e0f5d6cbdbc130835a506794158ff82a4203f287
abslan laverde <abslan.laverde@gmail.com>
2023-11-09 13:35:23 -0500

    initial commit

---
+++ src/main/java/com/abslanx/abslanxapi/AbslanxApiApplication.java
@@ -0,0 +1,13 @@
+package com.abslanx.abslanxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbslanxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbslanxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abslanx/abslanxapi/AbslanxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abslanx.abslanxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbslanxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
